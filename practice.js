7020e1428ae60d1390e0029a3f4e3caf7fd2d666
Schatfield <dgoldmine@icloud.com>
2020-08-20 15:28:44 -0500

    created a RegEx to find special characters and replace them in a string

--- practice.js
+++ practice.js
@@ -179,3 +179,17 @@ function sortArray2(array) {
     return is_odd(num) ? odd.shift() : num;
   }
 }
+
+
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+// Examples
+// toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+
+// toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+
+function toCamelCase(string) {
+  let re1 = new RegExp("-_");
+  let newString = string.replace(re1, "");
+  console.log(newString);
+}
